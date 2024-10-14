8b939a78b2325477c03b9c8af8fe64e4bd1026cd
Dexshine <b_o_y_s@hotmail.com>
2024-10-14 09:50:57 +0700

    route to new login page

--- src/routes/LoginRoutes.jsx
+++ src/routes/LoginRoutes.jsx
@@ -5,9 +5,10 @@ import MinimalLayout from "../layout/minimal";
 import GuestGuard from "../utils/route-guard/GuestGuard";
 import { Outlet } from "react-router-dom";
 import AuthCMU from "../pages/auth-cmu";
+import LoginPage from "../pages/login";
 
 // login routing
-const LoginPage = lazy(() => import("../pages/login"));
+// const LoginPage = lazy(() => import("../pages/login"));
 
 // ==============================|| AUTH ROUTING ||============================== //
 
